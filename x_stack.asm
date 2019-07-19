120b92b4e43f796f4f6adf9a0c3aa2d85a48f5f2
Freshnuts <fr3shnuts@gmail.com>
2019-07-19 00:51:41 -0700

    formatted correctly

--- x_stack.asm
+++ x_stack.asm
@@ -3,11 +3,11 @@ global _start
 
 _start:
 
-		; push '/home/orw/flag' onto stack.
-		push 0x00006761				; ag\x00\x00
-		push 0x6c662f77				; w/fl
-		push 0x726f2f65				; e/or
-ESP	->	push 0x6d6f682f				; /hom
+	; push '/home/orw/flag' onto stack.
+	push 0x00006761				; ag\x00\x00
+	push 0x6c662f77				; w/fl
+	push 0x726f2f65				; e/or
+ESP->	push 0x6d6f682f				; /hom
 
         ; 5 sys_open('filepath', 0)
         mov ecx, 0                  ; 0 read-only | 1 write-only | 2 read-write
