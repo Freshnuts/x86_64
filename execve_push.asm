a22d045b18874b77bb054bc7790ce322f535ee15
root <fr3shnuts@gmail.com>
2019-07-22 14:49:04 -0700

    added syscall execv()

--- execve_push.asm
+++ execve_push.asm
@@ -7,9 +7,9 @@ _start:
 	push 0x0068732f				; /sh\x00\x00
 	push 0x6e69622f				; /bin
 
-        ; 11 execve('/bin/sh\x00', 0, 0)
-        mov edx, 0                ; size to read
-        mov ecx, 0               	; file contents
-        mov ebx, esp                  ; print to stdout
+        ; 11 execv('/bin/sh\x00', 0, 0)
+        mov edx, 0
+        mov ecx, 0
+        mov ebx, esp			; move '/bin/sh' to ebx register.
         mov eax, 11
         int 0x80
